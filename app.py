0f558c8a979b6b3cdb4788ec692d0fa1108f9b68
guilhermeduartels <guilherme.duarte@datarisk.io>
2024-08-07 17:34:48 -0300

    Primeira versão de app

---
+++ app.py
@@ -0,0 +1,75 @@
+import streamlit as st
+import uuid
+from model import rag_chain
+
+from langchain_community.chat_message_histories import ChatMessageHistory
+from langchain_core.chat_history import BaseChatMessageHistory
+from langchain_core.runnables.history import RunnableWithMessageHistory
+
+if 'session_id' not in st.session_state:
+    st.session_state.session_id = None
+if 'messages' not in st.session_state:
+    st.session_state.messages = []
+if 'store' not in st.session_state:
+    st.session_state.store = {}
+
+
+def get_session_history(session_id: str) -> BaseChatMessageHistory:
+    if session_id not in st.session_state.store:
+        st.session_state.store[session_id] = ChatMessageHistory()
+    return st.session_state.store[session_id]
+
+def start_chat():
+    session_id = str(uuid.uuid4())
+    st.session_state.session_id = session_id
+
+def restart_chat():
+    session_id = str(uuid.uuid4())
+    st.session_state.session_id = session_id
+    st.session_state.messages = []
+
+conversational_rag_chain = RunnableWithMessageHistory(
+    rag_chain,
+    get_session_history,
+    input_messages_key="input",
+    history_messages_key="chat_history",
+    output_messages_key="answer",
+)
+
+st.title("BBC Digital - Bot de Atendimento")
+
+if st.session_state.session_id:
+    st.sidebar.button('Reiniciar conversa', on_click=restart_chat)
+else:
+    st.sidebar.button('Iniciar conversa', on_click=start_chat)
+
+
+if st.session_state.session_id:
+
+    for message in st.session_state.messages:
+        with st.chat_message(message["role"]):
+            st.markdown(message["content"])
+
+    if input := st.chat_input(placeholder = "Digite aqui sua pergunta:"):
+
+        st.session_state.messages.append({"role": "user", "content": input})
+        with st.chat_message("user"):
+            st.markdown(input)
+        
+        config = {"configurable": {"session_id": st.session_state.session_id}}
+        answer = conversational_rag_chain.invoke(
+            {"input": input},
+            config=config
+        )['answer']
+
+        st.session_state.messages.append({"role": "assistant", "content": answer})
+        with st.chat_message("assistant"):
+            st.markdown(answer)
+
+        print("Messages")
+        print(st.session_state.messages)
+        print('\n-------------\n')
+        print("Session ID:", st.session_state.session_id)
+        print("Session History")
+        print(get_session_history(st.session_state.session_id))
+        print('\n-------------\n')
---
+++ model.py
@@ -0,0 +1,57 @@
+import streamlit as st
+
+from langchain.chains import create_history_aware_retriever, create_retrieval_chain
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_chroma import Chroma
+from langchain_community.document_loaders import DirectoryLoader
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain_openai import ChatOpenAI, OpenAIEmbeddings
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+
+openai_api_key = st.secrets["OPENAI_API_KEY"]
+
+llm = ChatOpenAI(model="gpt-4o-mini", openai_api_key=openai_api_key)
+embedding = OpenAIEmbeddings(openai_api_key=openai_api_key)
+
+loader = DirectoryLoader('./md_files/', glob="*.md")
+docs = loader.load()
+
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=100)
+splits = text_splitter.split_documents(docs)
+vectorstore = Chroma.from_documents(documents=splits, embedding=embedding)
+retriever = vectorstore.as_retriever()
+
+contextualize_q_system_prompt = """
+Dado um histórico de chat e a última pergunta do usuário, que pode referenciar o contexto no histórico do chat, formule uma pergunta independente que possa ser compreendida sem o histórico do chat. 
+NÃO responda à pergunta, apenas reformule-a se necessário e, caso contrário, devolva-a como está.
+"""
+contextualize_q_prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", contextualize_q_system_prompt),
+        MessagesPlaceholder("chat_history"),
+        ("human", "{input}"),
+    ]
+)
+history_aware_retriever = create_history_aware_retriever(
+    llm, retriever, contextualize_q_prompt
+)
+
+qa_system_prompt = """
+Você é um assistente para tarefas de perguntas e respostas. 
+Use as seguintes partes do contexto recuperado para responder à pergunta. 
+Se você não souber a resposta, apenas diga que não sabe. 
+Use no máximo três frases e mantenha a resposta concisa.
+
+Contexto: {context}"""
+qa_prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", qa_system_prompt),
+        MessagesPlaceholder("chat_history"),
+        ("human", "{input}"),
+    ]
+)
+question_answer_chain = create_stuff_documents_chain(llm, qa_prompt)
+
+rag_chain = create_retrieval_chain(history_aware_retriever, question_answer_chain)
+
+
